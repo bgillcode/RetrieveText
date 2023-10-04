0a42148a501c895c73059330564792cb2a160e90
bgillcode <b.gill.messages@gmail.com>
2023-10-04 18:21:34 +0100

    Stop tracking apiUtil.ts

--- apiUtil.ts
+++
@@ -1,9 +0,0 @@
-export const constructTranslateApiUrl = (
-  sourceLang: string,
-  targetLang: string,
-): string => {
-  /* Replace constructedAPIURL with the API URL that's going to be used for translation,
-     along with sourceLang and targetLang if appropriate as part of the URL */
-  const constructedAPIURL = ``;
-  return constructedAPIURL;
-};
